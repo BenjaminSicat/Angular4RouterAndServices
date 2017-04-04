a151899a94d667f84b621be11604d7eecc93f6fb
Benjamin Sicat <benjamin.sicat@filamsoftware.com>
2017-04-04 16:21:32 +0800

    First commit

---
+++ ng2-router-services/src/app/classes/data-movies.ts
@@ -0,0 +1,60 @@
+import { Movie } from './movie';
+
+export const MOVIES: Movie[] = [
+    {
+        "title": "A New Hope",
+        "episode_id": 4,
+        "opening_crawl": "",
+        "director": "George Lucas",
+        "producer": "Gary Kurtz",
+        "release_date": "May 25, 1977",
+    },
+    {
+        "title": "The Empire Strikes Back",
+        "episode_id": 5,
+        "opening_crawl": "",
+        "director": "Irvin Kershner",
+        "producer": "Gary Kurtz",
+        "release_date": "May 21, 1980",
+    },
+    {
+        "title": "Return of the Jedi",
+        "episode_id": 6,
+        "opening_crawl": "",
+        "director": "Richard Marquand",
+        "producer": "Howard Kazanjian",
+        "release_date": "May 25, 1983",
+    },
+    {
+        "title": "The Phantom Menace",
+        "episode_id": 1,
+        "opening_crawl": "",
+        "director": "George Lucas",
+        "producer": "Rick McCallum",
+        "release_date": "May 19, 1999",
+    },
+    {
+        "title": "Attack of the Clones",
+        "episode_id": 2,
+        "opening_crawl": "",
+        "director": "George Lucas",
+        "producer": "Rick McCallum",
+        "release_date": "May 16, 2002",
+    },
+    {
+        "title": "Revenge of the Sith",
+        "episode_id": 3,
+        "opening_crawl": "",
+        "director": "George Lucas",
+        "producer": "Rick McCallum",
+        "release_date": "May 19, 2005",
+    },
+    {
+        "title": "The Force Awakens",
+        "episode_id": 7,
+        "opening_crawl": "",
+        "director": "J. J. Abrams",
+        "producer": "Kathleen Kennedy, J. J. Abrams & Bryan Burk",
+        "release_date": "December 18, 2015",
+    }
+];
